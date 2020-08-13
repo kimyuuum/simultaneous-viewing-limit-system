c297af15662a051e276f2b9e5e4539885b2e50cb
kimyuuum <yuuum.kim@kakao.com>
2020-08-13 17:56:40 +0900

    Create kafka consumer config

---
+++ check-service/src/main/java/com/skb/checkservice/config/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,52 @@
+package com.skb.checkservice.config.kafka;
+
+import com.skb.checkservice.dto.WatchInfoDto;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootStrapServers;
+
+    private static final String groupId = "watch-info-group";
+
+    @Bean
+    Map<String,Object> consumerConfigs(){
+      Map<String,Object> props = new HashMap<>();
+      props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootStrapServers);
+      props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+      props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+      props.put(ConsumerConfig.GROUP_ID_CONFIG,groupId);
+      return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, WatchInfoDto.Request> consumerFactory(){
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(),
+                                                 new StringDeserializer(),
+                                                 new JsonDeserializer<>(WatchInfoDto.Request.class,false));
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String,WatchInfoDto.Request>> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String,WatchInfoDto.Request> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
