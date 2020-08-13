9c1467e75b2665ac3b4151c51edb218ec582f98f
kimyuuum <yuuum.kim@kakao.com>
2020-08-13 17:58:24 +0900

    Create domain / dto for WatchInfo

---
+++ check-service/src/main/java/com/skb/checkservice/domain/WatchInfo.java
@@ -0,0 +1,28 @@
+package com.skb.checkservice.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class WatchInfo {
+
+    @JsonProperty
+    private String pcid;
+
+    @JsonProperty("episode_id")
+    private String episodeId;
+
+    @JsonProperty("stb_id")
+    private String stbId;
+
+    @JsonProperty("play_start")
+    private String playStart;
+
+    @JsonProperty("mac_address")
+    private String macAddress;
+
+}
