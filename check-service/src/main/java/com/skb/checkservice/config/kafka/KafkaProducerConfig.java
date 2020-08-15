9f70832c883857ffbc288e15936fc9039e098256
kimyuuum <yuuum.kim@kakao.com>
2020-08-15 21:14:50 +0900

    Create : checking exist user
    If user exists, first produce userExists topic
    Else insert user data to redis

---
+++ check-service/src/main/java/com/skb/checkservice/config/kafka/KafkaProducerConfig.java
@@ -0,0 +1,47 @@
+package com.skb.checkservice.config.kafka;
+
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+@EnableConfigurationProperties(KafkaProperties.class)
+public class KafkaProducerConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootStrapServers;
+
+    @Bean
+    public Map<String,Object> stringProducerConfigs(){
+        Map<String,Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootStrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String,String> stringProducerFactory(){
+        return new DefaultKafkaProducerFactory<>(stringProducerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String,String> kafkaTemplate(){
+        return new KafkaTemplate<>(stringProducerFactory());
+    }
+
+}
--- check-service/src/main/java/com/skb/checkservice/service/RedisClusterService.java
+++ check-service/src/main/java/com/skb/checkservice/service/RedisClusterService.java
@@ -2,7 +2,7 @@ package com.skb.checkservice.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skb.checkservice.domain.User;
-import com.skb.checkservice.dto.UserDto;
+import com.skb.checkservice.dto.WatchInfoDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -21,13 +21,27 @@ public class RedisClusterService {
         this.redisTemplate = redisTemplate;
     }
 
-    public User create(UserDto.Request dto){
-        String stbId = dto.getStbId();
-        String episodeId = dto.getEpisodeId();
+    public User create(WatchInfoDto.Request dto){
+        User newUser = User.builder()
+                            .pcid(dto.getPcid())
+                            .episodeId(dto.getEpisodeId())
+                            .stbId(dto.getStbId())
+                            .playStart(dto.getPlayStart())
+                            .macAddress(dto.getMacAddress())
+                            .running(true)
+                            .build();
 
-        redisTemplate.opsForHash().put(stbId,episodeId,dto.toEntity());
-        return objectMapper.convertValue(redisTemplate.opsForHash().get(stbId,episodeId),User.class);
+        redisTemplate.opsForHash().put(dto.getStbId(), dto.getEpisodeId(),newUser);
+        return objectMapper.convertValue(redisTemplate.opsForHash().get(dto.getStbId(),dto.getEpisodeId()),User.class);
     }
 
-
+    public boolean checkIsRunning(String stbId, String episodeId){
+        //if VOD Log presents check is running
+        if(redisTemplate.opsForHash().hasKey(stbId,episodeId)){
+            User user = objectMapper.convertValue(redisTemplate.opsForHash().get(stbId,episodeId),User.class);
+            return user.isRunning();
+        }else{
+            return false;
+        }
+    }
 }
