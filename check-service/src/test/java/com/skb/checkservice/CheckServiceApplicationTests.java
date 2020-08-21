33ec698f8fe107ed45108dfc7b13ec382903f3ea
kimyuuum <yuuum.kim@kakao.com>
2020-08-22 02:17:55 +0900

    Update : update test code

--- check-service/src/test/java/com/skb/checkservice/CheckServiceApplicationTests.java
+++ check-service/src/test/java/com/skb/checkservice/CheckServiceApplicationTests.java
@@ -6,10 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-
 
 
 @SpringBootTest
@@ -19,20 +17,19 @@ class CheckServiceApplicationTests {
     @Autowired
     private RedisTemplate<String, WatchInfo> redisTemplate;
 
-    @Qualifier("serializingObjectMapper")
     @Autowired
     ObjectMapper objectMapper;
 
     private WatchInfo newUser() {
         return WatchInfo.builder()
-                .macAddress("testMacNum")
-                .playStart("2010-10-10'T'08:03:02")
-                .playEnd("2020-10-10'T'12:01:01")
-                .stbId("stbId")
-                .episodeId("epId")
-                .pcid("pcId")
-                .running(true)
-                .build();
+                        .macAddress("testMacNum")
+                        .playStart("2010-10-10'T'08:03:02")
+                        .playEnd("2020-10-10'T'12:01:01")
+                        .stbId("stbId")
+                        .episodeId("epId")
+                        .pcid("pcId")
+                        .running(true)
+                        .build();
     }
 
     @Test
@@ -42,13 +39,15 @@ class CheckServiceApplicationTests {
         String stbId = "stbId";
         String episodeId = "epId";
 
-        redisTemplate.opsForHash().put(stbId, episodeId, newUser());
+        redisTemplate.opsForHash()
+                     .put(stbId, episodeId, newUser());
 
-        Object result = redisTemplate.opsForHash().get(stbId,episodeId);
-        WatchInfo user = objectMapper.convertValue(result,WatchInfo.class);
+        Object result = redisTemplate.opsForHash()
+                                     .get(stbId, episodeId);
+        WatchInfo user = objectMapper.convertValue(result, WatchInfo.class);
 
         Logger logger = LoggerFactory.getLogger(CheckServiceApplicationTests.class);
-        logger.info("{}",user);
+        logger.info("{}", user);
     }
 
 }
